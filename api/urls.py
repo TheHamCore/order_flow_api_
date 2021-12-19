bde9969e9a13ff196070d88248eb362ce9b91ad8
Semen <thehamcoree@gmail.com>
2021-12-19 10:50:45 +0300

    remove slash from DefaultRouter

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from .views import OrderViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register('orders', OrderViewSet, basename='Order')
 
 urlpatterns = router.urls
