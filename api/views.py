71a8c37235762f15ef0891a4fadc7f529971b656
Semen <thehamcoree@gmail.com>
2021-12-19 10:51:13 +0300

    add documentation string

--- api/views.py
+++ api/views.py
@@ -26,11 +26,58 @@ class OrderViewSet(ModelViewSet):
     ordering_fields = ['id', 'status', 'created_at']
 
     def retrieve(self, request, *args, **kwargs):
+        """
+        url: /api/v1/orders/{id}
+
+        response:
+            {
+                "id": 1,
+                "status": "new",
+                "created_at": "2021-01-01T00:00:00",
+                "external_id": "PR-123-321-123-new", # change this string
+                "details": [{
+                    "id": 1,
+                    "product": {"id": 4, "name": "Dropbox"},
+                    "amount": 10,
+                    "price": "12.00"
+                }]
+            }
+        """
         instance = self.get_object()
         serializer = OrderRetrieveSerializer(instance)
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
+        """
+        payload:
+        {
+            "external_id": "PR-123-321-123",
+            "details": [{
+                "product": {"id": 4, "name":"Dropbox"},
+                "amount": 10,
+                "price": "12.00"
+            }]
+        }
+
+        response:
+        {
+            "id": 5,
+            "status": "new",
+            "created_at": "2021-12-18T21:15:43.523149Z",
+            "external_id": "PR-123-321-123",
+            "details": [
+                {
+                    "id": 4,
+                    "amount": 10,
+                    "price": "12.00",
+                    "product": {
+                        "id": 4,
+                        "name": ""Dropbox""
+                    }
+                }
+            ]
+        }
+        """
         order = Order.objects.create(external_id=request.data['external_id'])
         for data in request.data['details']:
             product = Product.objects.create(name=data['product']['name'])
@@ -50,6 +97,56 @@ class OrderViewSet(ModelViewSet):
         return serializer_class
 
     def update(self, request, *args, **kwargs):
+        """
+        case 1: status = 'new'
+        payload:
+         {
+            "id": 1,
+            "status": "new",
+            "created_at": "2021-01-01T00:00:00",
+            "external_id": "PR-123-321-123-new", # change this string
+            "details": [{
+                "id": 1,
+                "product": {"id": 4, "name": "Dropbox"},
+                "amount": 10,
+                "price": "12.00"
+            }]
+        }
+
+        response:
+        {
+            "id": 1,
+            "status": "new",
+            "created_at": "2021-01-01T00:00:00",
+            "external_id": "PR-124-444-444-new", # change this string
+            "details": [{
+                "id": 1,
+                "product": {"id": 4, "name": "Dropbox"},
+                "amount": 10,
+                "price": "12.00"
+            }]
+        }
+
+        case 2: status = 'accepted' OR 'failed'
+        payload:
+         {
+            "id": 1,
+            "status": "accepted",
+            "created_at": "2021-01-01T00:00:00",
+            "external_id": "PR-123-321-123-new",
+            "details": [{
+                "id": 1,
+                "product": {"id": 4, "name": "Dropbox"},
+                "amount": 10,
+                "price": "12.00"
+            }]
+        }
+
+        response:
+        {
+            "error": "you cannot change data with status 'failed' or 'accepted'"
+        }
+        """
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
@@ -64,6 +161,25 @@ class OrderViewSet(ModelViewSet):
                             status=status.HTTP_403_FORBIDDEN)
 
     def destroy(self, request, *args, **kwargs):
+        """
+        case 1: status = 'new' OR 'failed'
+        url:
+            /api/v1/orders/{int}
+        response:
+            {
+                "success": "The data has deleted"
+            }
+
+
+        case 2: status = 'accepted'
+        url:
+            /api/v1/orders/{int}
+        response:
+            {
+                "error": "you cannot delete data with status 'accepted'"
+            }
+
+        """
         instance = self.get_object()
         if instance.status == 'accepted':
             return Response({"error": "you cannot delete data with status 'accepted'"},
@@ -75,6 +191,38 @@ class OrderViewSet(ModelViewSet):
 
     @action(methods=['post'], detail=True)
     def accept(self, request, pk=None):
+        """
+        This method allows changing status. Status "failed" changes to "accepted".
+
+        url request:
+            api/v1/orders/{id}/accept
+        payload:
+            {
+                "id": 1,
+                "status": "failed",
+                "created_at": "2021-01-01T00:00:00",
+                "external_id": "PR-123-321-123-new",
+                "details": [{
+                    "id": 1,
+                    "product": {"id": 4, "name": "Dropbox"},
+                    "amount": 10,
+                    "price": "12.00"
+                }]
+            }
+        response:
+            {
+                "id": 1,
+                "status": "accepted",
+                "created_at": "2021-01-01T00:00:00",
+                "external_id": "PR-123-321-123-new",
+                "details": [{
+                    "id": 1,
+                    "product": {"id": 4, "name": "Dropbox"},
+                    "amount": 10,
+                    "price": "12.00"
+                }]
+            }
+        """
         obj = self.get_object()
         obj.external_id = obj.external_id
         obj.status = obj.status
@@ -89,7 +237,40 @@ class OrderViewSet(ModelViewSet):
             return Response(serializer.data)
 
     @action(methods=['post'], detail=True)
+
     def fail(self, request, pk=None):
+        """
+        This method allows changing status. Status "accepted" changes to "failed".
+
+        url request:
+            api/v1/orders/{id}/fail
+        payload:
+            {
+                "id": 1,
+                "status": "accepted",
+                "created_at": "2021-01-01T00:00:00",
+                "external_id": "PR-123-321-123-new",
+                "details": [{
+                    "id": 1,
+                    "product": {"id": 4, "name": "Dropbox"},
+                    "amount": 10,
+                    "price": "12.00"
+                }]
+            }
+        response:
+            {
+                "id": 1,
+                "status": "failed",
+                "created_at": "2021-01-01T00:00:00",
+                "external_id": "PR-123-321-123-new",
+                "details": [{
+                    "id": 1,
+                    "product": {"id": 4, "name": "Dropbox"},
+                    "amount": 10,
+                    "price": "12.00"
+                }]
+            }
+        """
         obj = self.get_object()
         obj.external_id = obj.external_id
         obj.status = obj.status
